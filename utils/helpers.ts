e388443dc130b8236b1e43ecf8df03234f687d31
precious <ikpaprecious2@gmail.com>
2025-01-08 07:13:21 +0100

    updates

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -2,12 +2,12 @@ import { formatDistanceToNow } from "date-fns";
 
 export function formatRelativeTime(dateString: string): string {
   if (!dateString) {
-    return "Invalid date";
+    return "fetching date";
   }
 
   const date = new Date(dateString);
   if (isNaN(date.getTime())) {
-    return "Invalid date";
+    return "fetching date";
   }
 
   return formatDistanceToNow(date, { addSuffix: true });
